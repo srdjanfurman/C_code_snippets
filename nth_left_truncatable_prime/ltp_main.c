d1fb1dee3929dfa838fa73c5c0b58361bda407ba
srdjanfurman <furmanorama@gmail.com>
2020-05-16 17:59:07 +0200

    Code format.

--- nth_left_truncatable_prime/ltp_main.c
+++ nth_left_truncatable_prime/ltp_main.c
@@ -4,10 +4,10 @@
 #include <string.h>
 #include <time.h>
 
-#define MAX_LTP 999962683	/* 1000000000 */
+#define MAX_LTP 999962683	// 1000000000
 #define MAX_N_TH_LTP 2166
 
-/* Definition and variables for "elapsed time" testing purpose. */
+// Definition and variables for "elapsed time" testing purposes.
 #define NANOS 1000000000LL
 struct timespec ts_start;
 struct timespec ts_end;
@@ -16,38 +16,29 @@ long long end = 0;
 double time_val = 0.0;
 
 uint32_t cnt = 2;
-uint32_t ltp_array[MAX_N_TH_LTP] = {0};
+uint32_t ltp_array[MAX_N_TH_LTP] = { 0 };
 
-uint32_t check_prime(uint32_t n)
-{
+uint32_t check_prime(uint32_t n) {
 	uint32_t p;
-	if (n % 3 == 0)
-	{
+	if (n % 3 == 0) {
 		return 0;
 	}
 
-	for (p = 6; p * p <= n; p += 6)
-	{
-		if (!(n % (p + 1) && n % (p + 5)))
-		{
+	for (p = 6; p * p <= n; p += 6) {
+		if (!(n % (p + 1) && n % (p + 5))) {
 			return 0;
 		}
 	}
 	return 1;
 }
 
-void left_truncatable(uint32_t n, uint32_t tens)
-{
+void left_truncatable(uint32_t n, uint32_t tens) {
 	uint32_t k, lt_prime;
 	static uint32_t l = 0;
-	if (n < MAX_LTP / 10)
-	{
-		for (tens *= 10, k = 1; k < 10; k++)
-		{
-			if (check_prime(lt_prime = k * tens + n))
-			{
-				if (lt_prime)
-				{
+	if (n < MAX_LTP / 10) {
+		for (tens *= 10, k = 1; k < 10; k++) {
+			if (check_prime(lt_prime = k * tens + n)) {
+				if (lt_prime) {
 					ltp_array[l++] = lt_prime;
 				}
 				left_truncatable(lt_prime, tens);
@@ -57,15 +48,11 @@ void left_truncatable(uint32_t n, uint32_t tens)
 	cnt++;
 }
 
-void sort_in_asc()
-{
+void sort_in_asc() {
 	uint32_t i, j, tmp;
-	for (i = 0; i < cnt; ++i)
-	{
-		for (j = i+1; j < cnt; ++j)
-		{
-			if (ltp_array[i] > ltp_array[j])
-			{
+	for (i = 0; i < cnt; ++i) {
+		for (j = i + 1; j < cnt; ++j) {
+			if (ltp_array[i] > ltp_array[j]) {
 				tmp = ltp_array[i];
 				ltp_array[i] = ltp_array[j];
 				ltp_array[j] = tmp;
@@ -74,36 +61,32 @@ void sort_in_asc()
 	}
 }
 
-int main(int argc, char **argv)
-{
-	if (argc < 2)
-	{
-		printf ("Usage: Nth_left_truncatable_prime <nth_ltp>\n");
+int main(int argc, char **argv) {
+	if (argc < 2) {
+		printf("Usage: nth_left_truncatable_prime <nth_ltp>\n");
 		return 0;
 	}
 
 	uint16_t ref = 0;
 	ref = atoi(argv[1]);
-	if (ref > 2166)
-	{
+	if (ref > 2166) {
 		return 0;
 	}
 
-	// -------> Start measuring time ------->
-	clock_gettime (CLOCK_MONOTONIC, &ts_start);
+	// -------> Time measuring start ------->
+	clock_gettime(CLOCK_MONOTONIC, &ts_start);
 	left_truncatable(3, 1);
 	left_truncatable(7, 1);
 	sort_in_asc();
-	// <------- End measuring time <-------
-	clock_gettime (CLOCK_MONOTONIC, &ts_end);
+	// <------- Time measuring end <---------
+	clock_gettime(CLOCK_MONOTONIC, &ts_end);
 
 	// Display time results.
 	start = (ts_start.tv_sec * NANOS) + ts_start.tv_nsec;
 	end = (ts_end.tv_sec * NANOS) + ts_end.tv_nsec;
-	time_val = (end - start) / 1000000000.0;		/* duration in sec. */
-	printf("Time duration: %lf sec.\n", time_val);
-	printf("Number = %d\n", ltp_array[ref-1]);
+	time_val = (end - start) / 1000000000.0; // Duration in seconds.
+	printf("Time duration: %lf seconds.\n", time_val);
+	printf("Number = %d\n", ltp_array[ref - 1]);
 
 	return 0;
 }
-
